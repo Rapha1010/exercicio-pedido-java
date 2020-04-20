e3e4b4f1f8d4953553316e1847d804ad53ba6199
Raphael Tavares <rapha.tavares1010@gmail.com>
2020-04-20 20:11:26 -0300

    trabalho de pedidos

---
+++ src/entities/Order.java
@@ -0,0 +1,40 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	
+	private Date moment;
+	private OrderStatus orderStatus;
+	private List<OrderItem> orderItem = new ArrayList<>();
+	private Client client = new Client();
+	
+	public Order(Date moment, OrderStatus orderStatus) {
+		this.moment = moment;
+		this.orderStatus = orderStatus;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	
+	
+	
+
+}
